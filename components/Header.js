f957ca232bb8b6e5573793e8a9ea691e2da63646
kfiddle <kenjfiddle@gmail.com>
2021-05-12 23:09:13 -0400

    wired up

--- components/Header.js
+++ components/Header.js
@@ -5,9 +5,8 @@ const app = document.getElementById('app');
 export const Header = {
 
     render: function() {
-        console.log('I am the walrus');
         let header = BetterElement('header', 'header');
-        header.innerText = 'hello lamaaaaa';
+        header.innerHTML = `<div class="logoDiv"></div>`
         header.rollout("translateY(0vh)");
         app.appendChild(header);
 

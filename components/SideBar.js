31b19a8fe228b0c902b3f0b22873a1ec3f00b579
kfiddle <kenjfiddle@gmail.com>
2021-05-13 09:31:02 -0400

    side menu stuff

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,33 +1,58 @@
-import { BetterElement } from './BetterElement.js';
+import { BetterElement } from "./BetterElement.js";
 
-const app = document.getElementById('app');
+const app = document.getElementById("app");
 
-const menuItems = ['About Us', 'The Sculptures', 'The Artists', 'Getting Around', 'Sculptural Bike Racks', 'Hamilton', 'Contact Us']
+const menuItems = [
+  "About Us",
+  "The Sculptures",
+  "The Artists",
+  "Getting Around",
+  "Sculptural Bike Racks",
+  "Hamilton",
+  "Contact Us",
+];
 
+const NavItem = (navItemText) => {
+  let container = BetterElement("div", "navItemDiv");
+  let textElement = BetterElement("h2", "navBarItem");
+  textElement.innerText = navItemText;
+  container.appendChild(textElement);
+  container.addEventListener("mouseover", () => {
+    container.style.backgroundColor = "white";
+    textElement.style.color = "#c1272d";
+  });
+
+  container.addEventListener("mouseout", () => {
+    container.style.backgroundColor = "#c1272d";
+    textElement.style.color = "white";
+  });
+
+  return container;
+};
 
 export const SideBar = {
+  open: function () {
+    let sideBar = BetterElement("div", "sideBar");
 
-    open: function() {
-        let sideBar = BetterElement('div', 'sideBar');
+    menuItems.forEach(itemText => {
+        let navElements = NavItem(itemText);
+        sideBar.appendChild(navElements);
+    })
 
-        menuItems.forEach(item => {
-            let menuItem = document.createElement('h2', 'sideBarItem');
-            menuItem.innerText = item;
-            sideBar.appendChild(menuItem);
-
-
-
-
-
-
-        })
-        app.appendChild(sideBar);
-        setTimeout(()=> { sideBar.style.transform = 'translateX(10vw)'}, 100);
+   
+    app.appendChild(sideBar);
+    setTimeout(() => {
+      sideBar.style.transform = "translateX(10vw)";
+    }, 150);
+  },
+};
 
 
+// menuItems.forEach((item) => {
+//     let navItemDiv = BetterElement("div", "navItemDiv");
+//     let navBarItem = BetterElement("h2", "navBarItem");
+//     navBarItem.innerText = item;
 
-    }
-
-
-
-}
+//     navItemDiv.appendChild(navBarItem);
+//     sideBar.appendChild(navItemDiv);
+//   });

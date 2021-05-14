63b01e3918941554ed638d52252f97c891de8d3d
Karin Samoviski <karinsamoviski@karins-mbp.attlocal.net>
2021-05-14 16:26:02 -0400

    added fonts and cleaned up css

--- components/SideBar.js
+++ components/SideBar.js
@@ -22,7 +22,7 @@ const ourPanels = [
 
 const NavItem = (menuKeyValue) => {
   let container = BetterElement("div", "navItemDiv");
-  let textElement = BetterElement("h2", "navBarItem");
+  let textElement = BetterElement( 'h2',"navBarItem");
   textElement.innerText = menuKeyValue;
   container.appendChild(textElement);
   container.addEventListener("mouseenter", () => {

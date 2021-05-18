b146afebe0e27f903a27431e98894dfa5422055a
kfiddle <kenjfiddle@gmail.com>
2021-05-17 21:48:44 -0400

    getting there

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -5,6 +5,12 @@ const app = document.getElementById("app");
 const aboutUsStatement =
   "On August 16, 2000, Ohio’s governor declared and formally recognized Hamilton as “The City of Sculpture.” This sparked a vision for a group of community members who officially formed Hamilton, Ohio City of Sculpture, Inc. a 503c organization. Since that watershed event, a full Board of Trustees has been established and many new sculptures have been installed in public spaces around Hamilton. Other organizations and businesses are being energized through this vision and action.";
 
+const missionLink = BetterElement('button', 'missionAndBoard');
+const boardLink = BetterElement('button', 'missionAndBoard');
+
+missionLink.innerHTML = `<p class="missionAndBoardText">Our Mission</p>`;
+boardLink.innerHTML = `<p class="missionAndBoardText">Our Board</p>`;
+
 export const AboutUsPanel = {
   isOpen: false,
   title: "About Us",
@@ -16,14 +22,16 @@ export const AboutUsPanel = {
 
     extraSide.style.width = "27vw";
     extraSide.appendChild(xOut);
-
+    
     let statement = BetterElement("p", "aboutUsStatementDiv", "about");
     statement.innerText = aboutUsStatement;
     extraSide.appendChild(statement);
+    extraSide.appendChild(missionLink);
+    extraSide.appendChild(boardLink);
     app.appendChild(extraSide);
 
     setTimeout(() => {
-      extraSide.rollout("translateX(16vw)");
+      extraSide.rollout("translateX(17vw)");
     }, 10);
     this.isOpen = true;
   },
